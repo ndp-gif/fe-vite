cc47590dabc95e8aa078ce31e4e7801d8c000c98
nguyendinhphuong <nguyendinhphuong10a6qv@gmail.com>
2026-02-28 22:00:52 +0700

    refactor: cleanup examples and update App

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,7 @@
-import './App.css'
-import Exp from "./examples/exp"
-import Exp10 from './examples/Exp10'
-import Product from './examples/Product'
-import TodoApp from './examples/TodoApp'
-import WorkshopHeader from './examples/WorkshopHeader'
-
 function App() {  
+  
   return (
     <>
-    <WorkshopHeader/>
     
     </>
   )
